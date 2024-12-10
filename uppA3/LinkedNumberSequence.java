e32148babba1e680e023d709023fac62febf1264
hafshaafrin <94687156+hafshaafrin@users.noreply.github.com>
2024-12-10 16:16:42 +0100

    Update LinkedNumberSequence.java

--- uppA3/LinkedNumberSequence.java
+++ uppA3/LinkedNumberSequence.java
@@ -60,4 +60,190 @@ public class LinkedNumberSequence implements NumberSequence
 	}
 
     // add code here
+
+
+	public int length()
+	{
+		int length = 0;
+		if (first==null){
+			return (length);
+		}
+		Node nyaNode = first;
+		while (nyaNode != null){
+			length++;
+			nyaNode= nyaNode.next;
+		}
+		return (length);
+	}
+
+	public double upperBound (){
+		Node curr =first; 
+		double max= first.number;
+		while(curr != null){
+			if (curr.number > max){
+				max=curr.number;
+			}
+			curr=curr.next;
+		}
+		return (max);
+	}
+
+	public double lowerBound (){
+		Node curr=first; 
+		double min = first.number;
+		while(curr != null){
+			if (curr.number < min){
+				min = curr.number;
+			}
+			curr=curr.next;
+		}
+		return (min);
+	}
+	
+	public double numberAt (int position) throws IndexOutOfBoundsException
+	{
+		if (position == -1){
+			throw new IndexOutOfBoundsException("Finns inte");
+		}
+		Node n = first;
+		int pos= 0;
+		double nummer = 0;
+		while (n != null){
+			if (pos == position){
+				nummer=n.number;
+			}
+			pos ++;
+			n = n.next;
+		}
+		return (nummer);
+	}
+
+	public int positionOf (double number){
+		Node n = first;
+		int count = 0;// aktuella
+		int pos = -1;
+		while (n != null){
+			if (n.number == number){
+				pos= count;
+				break;
+			}
+			count ++;
+			n= n.next;
+		}
+		return (pos);
+	}
+
+	public boolean isIncreasing (){
+		Node n = first;
+    	while (n.next != null) {
+        	if (n.number >= n.next.number) {
+            	return false;
+       		}
+        n = n.next;
+    	}
+		return (true);
+	}
+
+	public boolean isDecreasing (){
+		Node n = first;
+    	while (n != null && n.next != null) {
+        	if (n.number < n.next.number) {
+            	return false;
+       		}
+        n = n.next;
+    	}
+		return (true);
+	}
+
+	public boolean contains (double number){
+		Node n = first;
+    	while (n != null){
+			if (n.number == number){
+				return (true);
+			}
+			n= n.next;
+		}
+		return (false);
+	}
+	
+	public void add (double number)
+	{
+		Node node = new Node (number);
+		if (first == null)
+			first = node;
+		else {
+			Node n = first;
+			while(n.next != null)
+				n= n.next; //sista noden
+			n.next = node;
+		}
+		/*Node n = first;
+		if (first == null){
+			first= node;
+		}
+		while (n.next != null){
+			n= n.next;
+		}
+		n.next = node;*/
+
+	}
+
+	public void insert (int position, double number)
+	throws IndexOutOfBoundsException
+	{
+		if (position < 0){
+			throw new IndexOutOfBoundsException("finns inte");
+		}
+		Node node = new Node (number);
+		if (position == 0){
+			node.next = first; //om fördta noden ska inser flyttar vi first
+			first = node;
+		}
+		else {
+			Node n = first;
+			for (int i = 0; i<position; i++){
+				n = n.next;
+			}
+			node.next = n.next;
+			n.next = node;
+		}
+	}
+
+	public void removeAt (int position)
+	throws IndexOutOfBoundsException, IllegalStateException
+	{
+		if (position < 0) {
+			throw new IndexOutOfBoundsException("finns inte");
+		}
+		if (length()<=1){
+			throw new IllegalStateException("bara ett element");
+		}
+		
+		if (position == 0){
+			first = first.next;
+		}
+		
+		Node n = first;
+		int i = 0;
+		while (n != null)
+		{
+			if (i == position-1){
+				n. next = n.next.next;
+			}
+			n=n.next;
+			i++;
+		}
+	}
+
+    public double[] asArray ()
+	{
+		Node n = first;
+		int length=length();
+		double[] array = new double[length];
+		for (int i = 0; i < length; i++) {
+			array[i]= n.number;
+			n = n.next;
+		}
+		return (array);
+	}
 }
